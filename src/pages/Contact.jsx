1101344678531cf57c630014b768ee21fcef697e
lilpinc <133043171+lilpinc@users.noreply.github.com>
2023-11-19 17:39:07 -0500

    make sure pictures work and resume is connected to download

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,53 @@
 import emailjs from '@emailjs/browser';
-import React, { useRef } from 'react';
-
+import React, { useRef, useState } from 'react';
+import { validateEmail } from "../utils/helpers.js";
 
 export default function Contact() {
 
   const form = useRef();
 
+  const [email, setEmail] = useState("");
+  const [name, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "user_email") {
+      setEmail(inputValue);
+    } else if (inputType === "user_name") {
+      setUserName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
   const sendEmail = (e) => {
     e.preventDefault();
+    if (!validateEmail(email) || !name) {
+      alert ("username or email is invalid")
+      return;
+    }
+    if (!setMessage(message)) {
+      alert ("Message is required.");
+      return;
+    }
     emailjs.sendForm('service_bwotmzn', 'template_q1tjkf6', form.current, 'YDOvY-Lk7P8bWkk1r')
-        .then((result) => {
-            console.log(result.text);
-            alert("message sent!")
-            // clear inputs
-        }, (error) => {
-            console.log(error.text);
-            console.log("error sending message, try again!")
-        });
-    };
-    
+      .then((result) => {
+        console.log(result.text);
+        alert("message sent!")
+        setUserName("");
+        setMessage("");
+        setEmail("");
+      }, (error) => {
+        console.log(error.text);
+        console.log("error sending message, try again!")
+      });
+  };
+
   return (
 
     <div className="contact">
@@ -27,28 +56,28 @@ export default function Contact() {
         <form action="" class="form" ref={form} onSubmit={sendEmail}>
           <div class="row">
             <label for="name">Your name:</label>
-            <input type="text" id="name" name="user_name" placeholder='name' required />
+            <input type="text" onChange={handleInputChange} id="name" value={name} name="user_name" placeholder='name' required />
           </div>
           <div class="row">
             <label for="email">Your email:</label>
-            <input type="email" id="email" name="user_email" placeholder='email' required />
+            <input type="email" onChange={handleInputChange} id="email" value={email} name="user_email" placeholder='email' required />
           </div>
           <div class="row">
             <label for="message">Your message:</label>
-            <textarea rows="4" id="message" name="user_message" placeholder='write message' />
+            <textarea rows="4" onChange={handleInputChange} id="message" value={message} name="user_message" placeholder='write message' />
           </div>
           <div class="row">
-            <button id="formbtn" type="submit" value="Send" >Submit</button>
+            <button onClick={sendEmail} id="formbtn" type="submit" value="Send" >Submit</button>
           </div>
         </form>
         <section class="mx-5 contactme">
           <p className="thanks">Thank you for checking out my website!</p>
           <p className="question">Want to get into contact? Reach me at the following:</p>
-            <ul className="contactlist">
+          <ul className="contactlist">
             <li>Phone: 757.536.5114</li>
             <li>Email: ab.pincus002@gmail.com</li>
-            </ul>
-            <p className="feedback">I'd love to hear your feedback!</p>
+          </ul>
+          <p className="feedback">I'd love to hear your feedback!</p>
         </section>
       </div>
     </div>
